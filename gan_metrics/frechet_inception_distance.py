f235cbe756700dba98a444dd56482bb28cf5f9ab
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2022-09-05 19:10:17 +0300

    Update frechet_inception_distance.py

--- gan_metrics/frechet_inception_distance.py
+++ gan_metrics/frechet_inception_distance.py
@@ -6,8 +6,9 @@ from scipy import linalg
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 
-from .base_class import BaseGanMetricComp
+from .base_class import BaseGanMetricStats
 from .inception_net import InceptionV3
+from .utils import Timer
 
 def torch_cov(m, rowvar=False):
     '''Estimate a covariance matrix given data.
@@ -60,18 +61,52 @@ def sqrt_newton_schulz(A, numIters, dtype=None):
     return sA
 
 
-class FID(BaseGanMetricComp):
+class FID(BaseGanMetricStats):
 
-    def calc_stats(self, loader, batch_size=50, use_torch=False, device: torch.device = torch.device('cuda:0')): # add loader
-        model = InceptionV3([InceptionV3.BLOCK_INDEX_BY_DIM[2048]]).to(device)
-        acts = model.get_features(loader, dim=2048, use_torch=use_torch)
-        if use_torch:
+    """
+    Class for Frechet Inception Distance calculation
+    """
+
+    def __init__(
+        self,
+        train_loader: torch.utils.data.DataLoader,
+        test_loader: torch.utils.data.DataLoader
+        ):
+        """
+        Args:
+            train_loader: generated data
+            test_loader: real world data
+        """
+
+        super().__init__(train_loader, test_loader)
+        self.inception_model = InceptionV3([InceptionV3.BLOCK_INDEX_BY_DIM[2048]])
+
+    def calc_stats(
+        self,
+        loader: torch.utils.data.DataLoader,
+        batch_size: int=50,
+        device: torch.device = torch.device('cuda:0')
+        ) -> Tuple[torch.FloatTensor, torch.FloatTensor, dict]:
+        """
+        Calculates statistics of inception features
+
+        Args:
+            loader: real world or generated data
+            batch_size: number of objects in one batch
+            device: what model device to use
+        returns:
+            mean and covariance of inception features
+            dictionary with time information
+
+        """
+
+        time_info = {}
+        with Timer(time_info, "FID: calc_stats"):
+            with Timer(time_info, "InceptionV3: get_features"):
+                acts = self.inception_model.get_features(loader, dim=2048, device=device)
             mu = torch.mean(acts, axis=0)
             sigma = torch_cov(acts)
-        else:
-            mu = np.mean(acts, axis=0)
-            sigma = np.cov(acts, rowvar=False)
-        return (mu, sigma)
+        return mu, sigma, time_info
 
     def calc_fd_in_np(
         self,
@@ -80,39 +115,54 @@ class FID(BaseGanMetricComp):
         m2: np.ndarray,
         s2: np.ndarray,
         eps: float = 1e-6
-    ) -> float:
+    ) -> Tuple[float, dict]:
+        """
+        Calculates Frechet DIstance betweet two distributions
 
-        m1 = np.atleast_1d(m1)
-        m2 = np.atleast_1d(m2)
+        Args:
+            m1: mean of first distribution
+            s1: covariance matrix of first distribution
+            m2: mean of second distribution
+            s2: covariance matrix of second distribution
+            eps: prevent covmean from being singular matrix
+        returns:
+            frechet distance between two distributions
+            dictionry with time information
+        """
 
-        s1 = np.atleast_2d(s1)
-        s2 = np.atleast_2d(s2)
+        time_info = {}
+        with Timer(time_info, "FID: calc_fd_in_np"):
+            m1 = np.atleast_1d(m1)
+            m2 = np.atleast_1d(m2)
 
-        diff = m1 - m2
+            s1 = np.atleast_2d(s1)
+            s2 = np.atleast_2d(s2)
 
-        # Product might be almost singular
-        covmean, _ = linalg.sqrtm(s1.dot(s2), disp=False)
-        if not np.isfinite(covmean).all():
-            msg = ('fid calculation produces singular product; '
-                'adding %s to diagonal of cov estimates') % eps
-            print(msg)
-            offset = np.eye(s1.shape[0]) * eps
-            covmean = linalg.sqrtm((s1 + offset).dot(s2 + offset))
+            diff = m1 - m2
 
-        # Numerical error might give slight imaginary component
-        if np.iscomplexobj(covmean):
-            if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
-                m = np.max(np.abs(covmean.imag))
-                raise ValueError('Imaginary component {}'.format(m))
-            covmean = covmean.real
+            # Product might be almost singular
+            covmean, _ = linalg.sqrtm(s1.dot(s2), disp=False)
+            if not np.isfinite(covmean).all():
+                msg = ('fid calculation produces singular product; '
+                    'adding %s to diagonal of cov estimates') % eps
+                print(msg)
+                offset = np.eye(s1.shape[0]) * eps
+                covmean = linalg.sqrtm((s1 + offset).dot(s2 + offset))
 
-        tr_covmean = np.trace(covmean)
+            # Numerical error might give slight imaginary component
+            if np.iscomplexobj(covmean):
+                if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+                    m = np.max(np.abs(covmean.imag))
+                    raise ValueError('Imaginary component {}'.format(m))
+                covmean = covmean.real
 
-        fd = (diff.dot(diff) +
-            np.trace(s1) +
-            np.trace(s2) -
-            2 * tr_covmean)
-        return fd
+            tr_covmean = np.trace(covmean)
+
+            fd = (diff.dot(diff) +
+                np.trace(s1) +
+                np.trace(s2) -
+                2 * tr_covmean)
+        return fd, time_info
 
     def calc_fd_in_torch(
         self,
@@ -121,19 +171,34 @@ class FID(BaseGanMetricComp):
         m2: torch.Tensor,
         s2: torch.Tensor,
         eps: float = 1e-6
-    ) -> float:
+    ) -> Tuple[float, dict]:
+        """
+        Calculates Frechet DIstance betweet two distributions
 
-        diff = m1 - m2
-        # Run 50 itrs of newton-schulz to get the matrix sqrt of (sigma1 x sigma2)
-        covmean = sqrt_newton_schulz(s1.mm(s2).unsqueeze(0), 50)
-        if torch.any(torch.isnan(covmean)):
-            return float('nan')
-        covmean = covmean.squeeze()
-        fd = (diff.dot(diff) +
-            torch.trace(s1) +
-            torch.trace(s2) -
-            2 * torch.trace(covmean)).cpu().item()
-        return fd
+        Args:
+            m1: mean of first distribution
+            s1: covariance matrix of first distribution
+            m2: mean of second distribution
+            s2: covariance matrix of second distribution
+            eps: prevent covmean from being singular matrix
+        returns:
+            frechet distance between two distributions
+            dictionry with time information
+        """
+
+        time_info = {}
+        with Timer(time_info, "FID: calc_fd_in_torch"):
+            diff = m1 - m2
+            # Run 50 itrs of newton-schulz to get the matrix sqrt of (sigma1 x sigma2)
+            covmean = sqrt_newton_schulz(s1.mm(s2).unsqueeze(0), 50)
+            if torch.any(torch.isnan(covmean)):
+                return float('nan')
+            covmean = covmean.squeeze()
+            fd = (diff.dot(diff) +
+                torch.trace(s1) +
+                torch.trace(s2) -
+                2 * torch.trace(covmean)).cpu().item()
+        return fd, time_info
 
     def calc_metric(
         self,
@@ -141,27 +206,49 @@ class FID(BaseGanMetricComp):
         test_stats_pth: str = None,
         use_torch: bool = False,
         eps: float = 1e-6
-    ) -> float:
+    ) -> Tuple[float, dict]:
+        """
+        Calculates Frechet Inception Distance betweet two datasets
 
-        if train_stats_pth is None:
-            m1, s1 = self.calc_stats(self.train_loader, use_torch=use_torch)
-        else:
-            f = np.load(train_stats_pth)
-            m1, s1 = f['mu'][:], f['sigma'][:]
-            f.close()
+        Args:
+            train_stats_pth: path to generated data statistics
+            test_stats_pth: path to real world data statistics
+            use_torch: whethere to use pytorch or numpy for frechet distance calculation
+            eps: prevent covmean from being singular matrix
+        returns:
+            frechet inception distance
+            dictionary with time information
+        """
 
-        if test_stats_pth is None:
-            m2, s2 = self.calc_stats(self.test_loader, use_torch=use_torch)
-        else:
-            f = np.load(test_stats_pth)
-            m2, s2 = f['mu'][:], f['sigma'][:]
-            f.close()
+        time_info = {}
+        with Timer(time_info, "FID: calc_metric"):
 
-        assert m1.shape == m2.shape, \
-            'Training and test mean vectors have different lengths'
-        assert s1.shape == s2.shape, \
-            'Training and test covariances have different dimensions'
+            if train_stats_pth is None:
+                with Timer(time_info, "FID: calc_stats, train"):
+                    m1, s1, calc_stats_time = self.calc_stats(self.train_loader)
+                time_info.update(calc_stats_time)
+            else:
+                f = np.load(train_stats_pth)
+                m1, s1 = f['mu'][:], f['sigma'][:]
+                f.close()
 
-        if use_torch:
-            return self.calc_fd_in_torch(m1, s1, m2, s2)
-        return self.calc_fd_in_np(m1, s1, m2, s2)
+            if test_stats_pth is None:
+                with Timer(time_info, "FID: calc_stats, test"):
+                    m2, s2, calc_stats_time = self.calc_stats(self.test_loader)
+                time_info.update(calc_stats_time)
+            else:
+                f = np.load(test_stats_pth)
+                m2, s2 = f['mu'][:], f['sigma'][:]
+                f.close()
+
+            assert m1.shape == m2.shape, \
+                'Training and test mean vectors have different lengths'
+            assert s1.shape == s2.shape, \
+                'Training and test covariances have different dimensions'
+
+            if use_torch:
+                fid = self.calc_fd_in_torch(m1, s1, m2, s2, eps)[0]
+            else:
+                fid = self.calc_fd_in_np(m1.cpu().numpy(), s1.cpu().numpy(), m2.cpu().numpy(), s2.cpu().numpy(), eps)[0]
+
+        return fid, time_info
