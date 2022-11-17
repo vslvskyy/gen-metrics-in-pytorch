b5fa94178272d383cecf58887e19b09e9521de9f
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2022-11-17 13:31:40 +0300

    train->generated, test->real

--- gan_metrics/base_class.py
+++ gan_metrics/base_class.py
@@ -1,17 +1,18 @@
-import torch
-
 from typing import Union
+
+from torch.utils.data import DataLoader
+
 
 class BaseGanMetric(object):
     """
     Base class for GAN's metric calculation
     """
 
-    def calc_metric(self):
+    def calc_metric(self, generated_data: Union[DataLoader, str]):
         """
         Computes metric value
         """
-        raise NotImplementedError('calc_metric is not implemented')
+        raise NotImplementedError("calc_metric is not implemented")
 
 
 class BaseGanMetricStats(BaseGanMetric):
@@ -20,19 +21,18 @@ class BaseGanMetricStats(BaseGanMetric):
     real world data statistics
     """
 
-    def __init__(self, test_data: Union[torch.utils.data.DataLoader, str]):
-        if isinstance(test_data, torch.utils.data.DataLoader):
-            self.test_loader = test_data
-            self.test_stats_pth = None
-        elif isinstance(test_data, str):
-            self.test_loader = None
-            self.test_stats_pth = test_data
+    def __init__(self, real_data: Union[DataLoader, str]):
+        if isinstance(real_data, DataLoader):
+            self.real_data_loader = real_data
+            self.real_data_stats_pth = None
+        elif isinstance(real_data, str):
+            self.real_data_loader = None
+            self.real_data_stats_pth = real_data
         else:
-            raise TypeError
+            raise TypeError("real_data should be DataLoader or str")
 
-
-    def get_stats(self):
+    def calc_stats(self, loader: DataLoader):
         """
         Calculates distrubution's statistics
         """
-        raise NotImplementedError('get_stats is not implemented')
+        raise NotImplementedError("calc_stats is not implemented")
