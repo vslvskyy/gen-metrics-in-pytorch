14fbdf2ddaca597463194502310cac5ac4ad4392
vslvskyy <princess05102001@gmail.com>
2022-12-09 12:54:56 +0300

    import fixes

--- gan_metrics/__init__.py
+++ gan_metrics/__init__.py
@@ -1,5 +1,5 @@
-from gan_metrics_in_pytorch.inception_score import InceptionScore
-from gan_metrics_in_pytorch.frechet_inception_distance import FID
+from gan_metrics.inception_score import InceptionScore
+from gan_metrics.frechet_inception_distance import FID
 
 __all__ = [
     InceptionScore, FID
