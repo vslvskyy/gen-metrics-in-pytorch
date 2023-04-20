c166e439dbeb6da5ed02516a486a6d05091b9d2d
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2023-04-21 00:06:30 +0300

    black format

--- gan_metrics/utils/__init__.py
+++ gan_metrics/utils/__init__.py
@@ -1,25 +1,15 @@
-from .other_utils import (
-    Registry,
-    sqrt_newton_schulz,
-    Timer,
-    torch_cov
-)
+from .other_utils import Registry, Timer, sqrt_newton_schulz, torch_cov
 
 from .datasets import datasets
 
-from .transforms import (
-    base_transformer,
-    clean_fid_transformer
-)
+from .transforms import base_transformer, clean_fid_transformer
 
 __all__ = [
     base_transformer,
     clean_fid_transformer,
-    
     datasets,
-
     Registry,
-    sqrt_newton_schulz,
     Timer,
-    torch_cov
+    sqrt_newton_schulz,
+    torch_cov,
 ]
