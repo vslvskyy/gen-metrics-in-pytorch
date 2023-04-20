eeac47d1e5379c2556c4cb741d0198efdfe48964
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2023-04-21 00:04:00 +0300

    black format

--- gan_metrics/inception_score.py
+++ gan_metrics/inception_score.py
@@ -15,12 +15,7 @@ class InceptionScore(BaseGanMetric):
     Class for Inception Score calculation
     """
 
-    def __init__(
-            self,
-            device = torch.device('cuda:0'),
-            splits_n: int = 10,
-            **kwargs
-        ):
+    def __init__(self, device=torch.device("cuda:0"), splits_n: int = 10, **kwargs):
         """
         Args:
             splits_n: number of batchs to calculate IS for each one
@@ -29,13 +24,11 @@ class InceptionScore(BaseGanMetric):
 
         self.device = device
         self.splits_n = splits_n
-        self.inception_model = InceptionV3([InceptionV3.BLOCK_INDEX_BY_DIM[1008]] , resize_input=False)
+        self.inception_model = InceptionV3(
+            [InceptionV3.BLOCK_INDEX_BY_DIM[1008]], resize_input=False
+        )
 
-
-    def calc_is(
-            self,
-            probs: torch.Tensor
-        ) -> Tuple[float, dict]:
+    def calc_is(self, probs: torch.Tensor) -> Tuple[float, dict]:
         """
         Batch Inception Score calculation
 
@@ -48,20 +41,17 @@ class InceptionScore(BaseGanMetric):
 
         time_info = {}
         with Timer(time_info, "InceptionScore: calc_is"):
-
             kl = probs * (
-                        torch.log(probs) -
-                        torch.log(torch.unsqueeze(torch.mean(probs, 0), 0)))
+                torch.log(probs) - torch.log(torch.unsqueeze(torch.mean(probs, 0), 0))
+            )
             kl = torch.mean(torch.sum(kl, 1))
             res = torch.exp(kl)
 
         return res, time_info
 
-
     def calc_metric(
-            self,
-            generated_data: Union[DataLoader, str]
-        ) -> Tuple[float, float, dict]:
+        self, generated_data: Union[DataLoader, str]
+    ) -> Tuple[float, float, dict]:
         """
         Inception Score calculation
 
@@ -72,6 +62,7 @@ class InceptionScore(BaseGanMetric):
             mean and standard deviation of batchs' IS
             dictionary with time information
         """
+
         if isinstance(generated_data, DataLoader):
             loader = generated_data
             probs_pth = None
@@ -79,20 +70,28 @@ class InceptionScore(BaseGanMetric):
             loader = None
             probs_pth = generated_data
         else:
-            raise TypeError("InceptionScore.calc_metric: generated_data should be DataLoader or str")
+            raise TypeError(
+                "InceptionScore.calc_metric: generated_data should be DataLoader or str"
+            )
 
         time_info = {}
         with Timer(time_info, "InceptionScore: calc_metric"):
-
             if probs_pth is None:
                 with Timer(time_info, "InceptionV3: get_features"):
-                    probs = self.inception_model.get_features(loader, dim=1008, device=self.device)
+                    probs = self.inception_model.get_features(
+                        loader, dim=1008, device=self.device
+                    )
             else:
                 probs = np.load(probs_pth)
 
             scores = []
             with Timer(time_info, "InceptionScore: total_calc_is"):
-                for i, batch in enumerate(torch.split(probs, split_size_or_sections=probs.shape[0]//self.splits_n), start=0):
+                for i, batch in enumerate(
+                    torch.split(
+                        probs, split_size_or_sections=probs.shape[0] // self.splits_n
+                    ),
+                    start=0,
+                ):
                     score = self.calc_is(batch)[0]
                     scores.append(score)
 
@@ -100,6 +99,8 @@ class InceptionScore(BaseGanMetric):
                 inception_score = torch.mean(scores).cpu().item()
                 std = torch.std(scores).cpu().item()
 
-            time_info["duration/InceptionScore: mean_calc_is"] = time_info["duration/InceptionScore: total_calc_is"]/self.splits_n
+            time_info["duration/InceptionScore: mean_calc_is"] = (
+                time_info["duration/InceptionScore: total_calc_is"] / self.splits_n
+            )
 
-        return inception_score, std, time_info
+        return (inception_score, std), time_info
