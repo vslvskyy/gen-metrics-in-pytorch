f2b8302e53463526bf485c9da6bfca04cc97b0c6
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2023-05-02 00:04:37 +0300

    Update __init__.py

--- gan_metrics/__init__.py
+++ gan_metrics/__init__.py
@@ -1,6 +1,5 @@
 from .inception_score import InceptionScore
-from .frechet_inception_distance import FID
+from .frechet_inception_distance import FID, CLEANFID, FIDNUMPY
+from .improved_prd import ImprovedPRD
 
-__all__ = [
-    InceptionScore, FID, ImprovedPRD
-]
+__all__ = [InceptionScore, FID, CLEANFID, FIDNUMPY, ImprovedPRD]
