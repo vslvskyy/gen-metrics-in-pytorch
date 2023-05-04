988a7a811641a0db05066c5cd16204cb5ad3109f
vslvskyy <75453192+vslvskyy@users.noreply.github.com>
2023-05-04 23:16:11 +0300

    Update __init__.py

--- gen_metrics/__init__.py
+++ gen_metrics/__init__.py
@@ -1,5 +1,5 @@
 from .inception_score import InceptionScore
-from .frechet_inception_distance import FID, CLEANFID, FIDNUMPY
+from .frechet_inception_distance import Fid, CleanFid, FidNumpy
 from .precision_recall import ImprovedPRD
 
-__all__ = [InceptionScore, FID, CLEANFID, FIDNUMPY, ImprovedPRD]
+__all__ = [InceptionScore, Fid, CleanFid, FidNumpy, ImprovedPRD]
